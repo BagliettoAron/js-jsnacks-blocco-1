3ca3878b863011a83e26e76e99a2710878d62021
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-19 16:46:29 +0200

    Fix: comment in jsnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,16 +1,17 @@
 // jsnack3. Calcola la somma e la media dei primi 10 numeri.
 
-// creo un array con i primi 10 numeri
-
-
+// reset della somma
 let somma = 0;
 
+// creo i 10 numeri con un loop for
 for (let i = 1; i <= 10; i++) {
     console.log("intervallo numeri incrementati", i);
+    // addiziono la somma per se stessa
     somma = somma + i
 }
 
 console.log("somma totale", somma);
 
+// calcolo la media
 const media = somma / 10
 console.log("media", media);
